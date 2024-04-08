c8092b79957c16150813875210cd9f58b0b88248
Yujie <yp25@st-andrews.ac.uk>
2024-04-08 17:55:14 +0100

    feat: add results animation using framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,7 @@ function App() {
 
   const onKeyDown = useCallback(
     (e: KeyboardEvent) => {
+      e.preventDefault()
       if (!isCountDownStart) {
         startCountDown()
       }
--- src/components/DisplayResult.tsx
+++ src/components/DisplayResult.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion"
 interface displayResult {
   resultArr: Array<boolean | number>
   isCountDownEnd: boolean
@@ -22,19 +23,52 @@ export default function DisplayResult({
     })
     const typedArr = [...resultArr].filter((i) => i !== 1)
     const correctRate = (correctWordCount / typedArr.length) * 100
+
+    const initial = { opacity: 0 }
+    const animate = { opacity: 1 }
+    const duration = { duration: 0.3 }
+
     return (
-      <div className="grid place-items-center mt-5">
-        <span className="text-primary-500 font-bold">Results</span>
-        <span className="text-primary-500">
+      <motion.ul className="flex flex-col items-center text-primary-400 space-y-3">
+        <motion.li
+          initial={initial}
+          animate={animate}
+          transition={{ ...duration, delay: 0 }}
+          className="font-bold"
+        >
+          Results
+        </motion.li>
+        <motion.li
+          initial={initial}
+          animate={animate}
+          transition={{ ...duration, delay: 0.5 }}
+        >
           {((typedArr.length / totalTime) * 60).toFixed(0)} characters per
           minute
-        </span>
-        <span className="text-primary-500">
+        </motion.li>
+        <motion.li
+          initial={initial}
+          animate={animate}
+          transition={{ ...duration, delay: 1.0 }}
+        >
           Accuracy: {correctRate.toFixed(1)} %
-        </span>
-        <span className="text-primary-500">Errors: {errorWordCount}</span>
-        <span className="text-primary-500">Typed: {resultArr.length}</span>
-      </div>
+        </motion.li>
+        <motion.li
+          initial={initial}
+          animate={animate}
+          transition={{ ...duration, delay: 1.5 }}
+          className="text-error-500"
+        >
+          Errors: {errorWordCount}
+        </motion.li>
+        <motion.li
+          initial={initial}
+          animate={animate}
+          transition={{ ...duration, delay: 2.0 }}
+        >
+          Typed: {resultArr.length}
+        </motion.li>
+      </motion.ul>
     )
   } else {
     return <></>
