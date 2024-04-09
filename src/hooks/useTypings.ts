47e0ac51d2a4186538c781e1de3b35b71df219e1
Yujie <yp25@st-andrews.ac.uk>
2024-04-09 13:05:45 +0100

    add useTyping hook

---
+++ src/hooks/useTypings.ts
@@ -0,0 +1,61 @@
+import { useCallback, useEffect, useRef, useState } from "react"
+
+const isKeyboardCodeAllowed = (code: string) => {
+    return (
+        code.startsWith('Key') || code.startsWith('Digit') || code === 'Backspace' || code === "Space"
+    )
+}
+
+
+const useTypings = (enabled: boolean) => {
+    const [cursor, setCursor] = useState(0)
+    const [typed, setTyped] = useState<string>("")
+    const totalTyped = useRef(0)
+
+    const keydownHandler = useCallback(({ key, code }: KeyboardEvent) => {
+        if (!enabled || !isKeyboardCodeAllowed(code)) {
+            return
+        }
+
+        switch (key) {
+            case 'Backspace':
+                setTyped(prev => prev.slice(0, -1)) // [firstElement, lastElement)
+                setCursor(cursor - 1)
+                totalTyped.current -= 1
+                break;
+            default:
+                setTyped(prev => prev.concat(key))
+                setCursor(cursor + 1)
+                totalTyped.current += 1
+        }
+    }, [cursor, enabled])
+
+
+    const clearTyped = useCallback(() => {
+        setTyped("")
+        setCursor(0)
+    }, [])
+
+    const resetTotalTyped = useCallback(() => {
+        totalTyped.current = 0
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener('keydown', keydownHandler)
+
+        return () => {
+            window.removeEventListener('keydown', keydownHandler)
+        }
+
+    }, [keydownHandler])
+
+    return {
+        typed,
+        cursor,
+        clearTyped,
+        resetTotalTyped,
+        totalTyped: totalTyped.current
+    }
+}
+
+export default useTypings
